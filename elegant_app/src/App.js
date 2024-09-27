497f8e58dc8ab610ab7e241e8d868b7da09ca20a
LucasPedemonte <144968398+LucasPedemonte@users.noreply.github.com>
2024-09-27 16:52:03 -0400

    Update

--- elegant_app/src/App.js
+++ elegant_app/src/App.js
@@ -1,32 +1,39 @@
 // src/App.js
 import React from "react";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
-import Home from "./Components/Home.js";
-import About from "./Components/AboutUs.js";
-import Contact from "./Components/ContactUs.js";
+import Home from "./Components/Home";
+import AboutUs from "./Components/AboutUs";
+import ContactUs from "./Components/ContactUs";
+import Pricing from "./Components/Pricing";
 
 function App() {
   return (
     <Router>
       <div>
+        {/* Navigation Tabs */}
         <nav>
           <ul>
             <li>
               <Link to="/">Home</Link>
             </li>
             <li>
-              <Link to="/about">About</Link>
+              <Link to="/about">About Us</Link>
             </li>
             <li>
-              <Link to="/contact">Contact</Link>
+              <Link to="/contact">Contact Us</Link>
+            </li>
+            <li>
+              <Link to="/pricing">Pricing</Link>
             </li>
           </ul>
         </nav>
 
+        {/* Routes to different components */}
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/contact" element={<ContactUs />} />
+          <Route path="/pricing" element={<Pricing />} />
         </Routes>
       </div>
     </Router>
